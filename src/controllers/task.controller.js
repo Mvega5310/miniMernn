cd9dfac1fb58812f4598a29fa4199d50fe69c214
Marcos <marcosvega5310@gmail.com>
2026-02-24 22:58:02 -0500

    Primer commit

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,28 @@
+import Task from '../models/task.js';
+
+export async function getTasks(req, res) {
+    const tasks = await Task.find().sort({ createdAt: -1 })
+    res.json(tasks)
+}
+
+export async function createTask(req, res) {
+    const {title} = req.body
+    if (!title) return res.status(400).json({message: 'el titulo es requerido'})
+
+        const task = await Task.create({title})
+        res.status(201).json(task)
+}
+
+export async function updateTask(req, res) {
+    const {id} = req.params
+    const update = await Task.findByIdAndUpdate(id, req.body, {new: true})
+    if (!update) return res.status(404).json({message: 'tarea no encontrada'})
+    res.json(update)
+}
+
+export async function deleteTask(req, res) {
+    const {id} = req.params
+    const deleted = await Task.findByIdAndDelete(id)
+    if (!deleted) return res.status(404).json({message: 'tarea no encontrada'})
+    res.json({ok: true})
+}
